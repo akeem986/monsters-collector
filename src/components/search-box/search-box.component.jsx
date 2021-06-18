051f9357351c74973d24cf9cf57c2fcf85739040
akeem986 <46425945+akeem986@users.noreply.github.com>
2021-06-18 18:19:13 -0400

    git progress

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './search-box.styles.css'
+
+const searchBox = ({placeholder, handleChange}) => {
+    return (
+        <div>
+        <input  className='search'
+        type='search' 
+        placeholder={placeholder}
+     onChange={handleChange}/>
+        </div>
+    );
+}
+
+export default searchBox;
